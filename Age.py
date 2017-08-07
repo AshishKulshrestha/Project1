5482e8519af2749363a72d61a37142dca8dec5e0
Ashish Kulshrestha <kulshrestha863@gmail.com>
2017-08-07 21:12:11 +0530

    Age after 100 yrs

---
+++ Age.py
@@ -0,0 +1,8 @@
+name = raw_input("Enter your name : ")
+print "Name inputted is : "+ name
+
+age = int(raw_input("Enter your age : "))
+print "Your age is :",age
+
+new_age = (100 - age) + 2017
+print "You will be 100 in the year ",new_age
