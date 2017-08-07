25b026d473ce10330a2951caee9db252c0e4120a
Ashish Kulshrestha <kulshrestha863@gmail.com>
2017-08-07 21:08:37 +0530

    Project1

---
+++ main.py
@@ -0,0 +1,14 @@
+print "Let us begin! "
+name = raw_input("Enter your name : ")
+length = len(name)
+if length > 0:
+    print "Name inputted is : "+ name
+else:
+    print "Name cannot be empty. Please enter your name."
+    name = raw_input("Enter your name : ")
+
+salutation = raw_input("Enter your salutation Mr. or Ms :")
+print "Welcome "+salutation+" "+name+". Let's get started with python."
+
+#Enter your age
+
