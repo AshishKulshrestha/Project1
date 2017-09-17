9825bead9395f84b8e39678a393e8f90c85151ac
Ashish Kulshrestha <kulshrestha863@gmail.com>
2017-09-18 02:45:39 +0530

    Adding new friend

--- Add_friend.py
+++ Add_friend.py
@@ -1,6 +1,8 @@
 # import statements.
+from Spy_detail import Spy
+import re
 from globals import friends
-
+spy = Spy("Star Boy","Mr",20,3.0,True)
 # add new friends.
 def add_friend():
     new_friend = {
@@ -11,22 +13,39 @@ def add_friend():
         'is_online': False,
         'chats': []
     }
-    new_friend['name'] = raw_input("Please add your friend's name: ")
-    new_friend['salutation']= raw_input("Are they Mr. or Ms.?: ")
-
-    # concatination.
-    new_friend['name'] = new_friend['salutation'] + " " + new_friend['name']
-
-    new_friend['age'] = int(raw_input("Age? "))
+    temp_variable = True
+    while temp_variable:
+        pattern = '^[a-zA-Z\s][a-zA-Z\s]*$'
+        new_friend['name'] = raw_input("Please add your friend's name: ")
+        if (re.match(pattern, new_friend['name'])):
+            temp_variable = False
+            print "Please provide some more information of yours."
+        else:
+            print "Please input only alphabetical characters."
+    temp_variable = True
+    while temp_variable:
+        new_friend['salutation']= raw_input("Are they Mr. or Ms.?: ")
+        pattern = '^ms|mr|MR|MS|Mr|Ms'
+        if (re.match(pattern, new_friend['salutation'])):
+           new_friend['name'] = new_friend['salutation'] + " " + new_friend['name']
+           temp_variable = False
+        else:
+            print "Please provide correct saluttion."
+    while True:
+        try:
+            new_friend['age'] = int(raw_input("Age? "))# converting users input to integer (typecasting)
+            break
+        except ValueError:
+            print "Stupid idiot! write a numeric value"
 
     new_friend['rating'] = float(raw_input("Spy rating? "))
 
     # users input validations
-    if len(new_friend['name']) > 0 and new_friend['age'] > 12 and new_friend['age'] < 50:
+    if len(new_friend['name']) > 0 and new_friend['age'] > 12 and new_friend['age'] < 50 and new_friend['rating']> spy.rating:
         friends.append(new_friend)
         print 'Friend Added!'
     else:
-        print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
+        print 'Sorry! Something went wrong. We can\'t add spy with the details you provided'
 
     # returning total no of friends.
     return len(friends)
