78d4418a3462d24219bf1c9c90f7e2f1825c6d7f
Ashish Kulshrestha <kulshrestha863@gmail.com>
2017-09-18 15:48:26 +0530

    Spy information

---
+++ Spy_detail.py
@@ -0,0 +1,8 @@
+class Spy:
+    def __init__(self,name,salutation,age,rating,is_online):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_Online = True
+        self.chat = []
