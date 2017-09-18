78319378487e3513d630f0bd59d46c80eec2e33a
Ashish Kulshrestha <kulshrestha863@gmail.com>
2017-09-18 15:45:21 +0530

    Reading message

--- read_message.py
+++ read_message.py
@@ -1,20 +1,29 @@
 from select_friend import select_friend
 from steganography.steganography import Steganography
 from datetime import datetime
+import re
 from globals import friends
 def read_message():
     # choose friend from the list
     sender = select_friend()
-
-    encrypted_image = raw_input("Provide encrypted image : ")
-    secret_message = Steganography.decode(encrypted_image)
-
-    # save the messages
-    new_chat = {
-        'message': secret_message,
-        'time': datetime.now(),
-        'sent_by_me': False
-    }
-
-    friends[sender]['chats'].append(new_chat)
-    print new_chat['message']
+    if sender < len(friends):
+        temp = True
+        while temp:
+            pattern = '^[a-zA-Z0-9]+\.jpg$'
+            encrypted_image = raw_input("Provide encrypted image : ")
+            if(re.match(pattern,encrypted_image)):
+                print "Valid image"
+                temp = False
+            else:
+                print "Invalid image"
+        secret_message = Steganography.decode(encrypted_image)
+        # save the messages
+        new_chat = {
+            'message': secret_message,
+            'time': datetime.now(),
+            'sent_by_me': False
+        }
+        friends[sender]['chats'].append(new_chat)
+        print new_chat['message']
+    else:
+        print "Wrong index entered"
